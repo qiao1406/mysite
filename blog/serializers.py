93d81af0e7f8b4b0e110b10640d2b0b67538037e
qiao1406 <lwt1406@buaa.edu.cn>
2021-11-20 15:19:24 +0800

    hyperlink and pages

--- blog/serializers.py
+++ blog/serializers.py
@@ -5,11 +5,12 @@ from user_info.serializers import UserDescSerializer
 
 class ArticleListSerializer(serializers.ModelSerializer):
     author = UserDescSerializer(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name='blog:detail')
 
     class Meta:
         model = Article
         fields = [
-            'id',
+            'url',
             'title',
             'created',
             'updated',
